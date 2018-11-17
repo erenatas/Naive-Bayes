48c42a92302149ed2164d1da36022e0d93236be0
erenatas <sovairon@gmail.com>
2018-11-17 18:26:30 +0300

    Update Project

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,148 +1,136 @@
-import math
-
+# Import Libs
+# conda install -c anaconda pandas
+from __future__ import division
 import pandas as pd
-import numpy as np
-import random
+import math
 from sklearn.utils import shuffle
+from pandas_ml import ConfusionMatrix
+
 
 # Get CSV
-columnNames=['Sex','Length','Diameter','Height','Whole weight','Shucked weight','Viscera weight','Shell weight','classlabel']
-train = pd.read_csv("abalone_dataset.txt",delimiter="\t", names=columnNames, header=None)
+columnNames = ['Sex', 'Length', 'Diameter', 'Height', 'WholeWeight', 'ShuckedWeight', 'VisceraWeight', 'ShellWeight',
+               'classlabel']
+train = pd.read_csv("abalone_dataset.txt", delimiter="\t", names=columnNames, header=None)
+
+
+# For continuous attributes, probability density function for Naive Bayes
+def p_x_given_y(x, mean_y, variance_y):
+    exponent = float(math.exp(-(math.pow(x - mean_y, 2) / (2 * variance_y))))
+    return float((1 / (math.sqrt(2 * math.pi * variance_y))) * exponent)
+
+
+# Transform Categorical Attributes to Continous Attributes.
+# I have used Hot Encoding to transform categorical attribute to binary attributes.
+def hotEncodeSex(data, test):
+    # Hot Encode Sex for Data and Test
+    sex_hot_encode_data = pd.get_dummies(data['Sex'])
+    data = data.drop('Sex', axis=1)
+    data = data.join(sex_hot_encode_data)
+    sex_hot_encode_test = pd.get_dummies(test['Sex'])
+    test = test.drop('Sex', axis=1)
+    test = test.join(sex_hot_encode_test)
+
+    return data, test
+
+
+# Find prior probabilities of class label.
+# Find mean and variance of the data.
+def preCalculations(data):
+    num_of1 = data['classlabel'][data['classlabel'] == 1].count()
+    num_of2 = data['classlabel'][data['classlabel'] == 2].count()
+    num_of3 = data['classlabel'][data['classlabel'] == 3].count()
+
+    total_class_label = data['classlabel'].count()
+
+    prior1 = num_of1 / total_class_label
+    prior2 = num_of1 / total_class_label
+    prior3 = num_of1 / total_class_label
+
+    data_mean = data.groupby('classlabel').mean()
+    data_variance = data.groupby('classlabel').var()
+
+    return prior1, prior2, prior3, data_mean, data_variance
+
+
+# Calculate the probabilities of the possibility of class label.
+# Class label types here are 1, 2 and 3.
+# After calculating, find the maximum probability for each of the calculated.
+# After that, calculate the accuracy.
+# First time I started writing this part, I tried not to use loops for hoping to have better runtime, but it was going
+# to be around 400 LOC so converted it to this function which is at O(nˆ3). It's not fast but does the job.
+def naiveBayes(attributes, data, test, expected_test):
+    data, test = hotEncodeSex(data, test)
+    prior1, prior2, prior3, data_mean, data_variance = preCalculations(data)
+    priors = [prior1, prior2, prior3]
+    class_label_types = [1, 2, 3]
+    output = []
+
+    for i in range(len(test)):
+        expected_result = expected_test['classlabel'].iloc[i]
+        max_of_class_labels = []
+        for classLabelType in range(len(class_label_types)):
+            probability_of_class_label_type = priors[classLabelType]
+            for attribute in range(len(attributes)):
+                mean_y = data_mean[attributes[attribute]][data_variance.index == classLabelType + 1].values[0]
+                variance_y = \
+                    data_variance[attributes[attribute]][
+                        data_variance.index == class_label_types[classLabelType]].values[0]
+                probability_of_class_label_type = probability_of_class_label_type * p_x_given_y(
+                    test[attributes[attribute]].iloc[i], mean_y, variance_y)
+            max_of_class_labels.append(probability_of_class_label_type)
+        output.append([max_of_class_labels.index(max(max_of_class_labels)) + 1, expected_result])
+    return accuracy(output)
+
+# For the following result
+def accuracy(result):
+    true = 0
+    total = len(result)
+    cm_expected = []
+    cm_predicted = []
+    for i in range(len(result)):
+        if result[i][0] == result[i][1]:
+            true += 1
+        cm_expected.append(result[i][1])
+        cm_predicted.append(result[i][0])
+    misclassified = total - true;
+    cm = ConfusionMatrix(cm_expected, cm_predicted)
+    cm.print_stats()
+    print("----------------------------------------")
+    return cm, total, true, misclassified, true/len(result)*100
+
 
 # Data Preparation for Cases
 train = shuffle(train)
 case1Data = train.iloc[:100, [0,1,2,8]]
 case1Test = train.iloc[101:, [0,1,2]]
 case1ExpectedTest = train.iloc[101:, [0,1,2,8]]
-train = shuffle(train)
 case2Data = train.iloc[:1000, [0,1,2,8]]
 case2Test = train.iloc[1001:, [0,1,2]]
-train = shuffle(train)
+case2ExpectedTest = train.iloc[1001:, [0,1,2,8]]
 case3Data = train.iloc[:2000, [0,1,2,8]]
-case3Test = train.iloc[2001:, [0,1,2,8]]
-train = shuffle(train)
+case3Test = train.iloc[2001:, [0,1,2]]
+case3ExpectedTest = train.iloc[2001:, [0,1,2,8]]
 case4Data = train.iloc[:100, :]
 case4Test = train.iloc[101:, :8]
-train = shuffle(train)
+case4ExpectedTest = train.iloc[101:, :]
 case5Data = train.iloc[:1000, :]
 case5Test = train.iloc[1001:, :8]
-train = shuffle(train)
+case5ExpectedTest = train.iloc[1001:, :]
 case6Data = train.iloc[:2000, :]
 case6Test = train.iloc[2001:, :8]
+case6ExpectedTest = train.iloc[2001:, :]
 
-
-def type1(data, test, expectedTest):
-    # Hot Encode Sex for Data and Test
-    sexHotEncodeData = pd.get_dummies(data['Sex'])
-    data = data.drop('Sex', axis=1)
-    data = data.join(sexHotEncodeData)
-    sexHotEncodeTest = pd.get_dummies(test['Sex'])
-    test = test.drop('Sex', axis=1)
-    test = test.join(sexHotEncodeTest)
-
-    numOf1 = data['classlabel'][data['classlabel'] == 1].count()
-    numOf2 = data['classlabel'][data['classlabel'] == 2].count()
-    numOf3 = data['classlabel'][data['classlabel'] == 3].count()
-
-    totalClassLabel = data['classlabel'].count()
-
-    prior1 = numOf1 / totalClassLabel
-    prior2 = numOf1 / totalClassLabel
-    prior3 = numOf1 / totalClassLabel
-
-    dataMean = data.groupby('classlabel').mean()
-    dataVariance = data.groupby('classlabel').var()
-
-    # Means for 1,2,3
-    oneFMean = dataMean['F'][dataVariance.index == 1].values[0]
-    oneIMean = dataMean['I'][dataVariance.index == 1].values[0]
-    oneMMean = dataMean['M'][dataVariance.index == 1].values[0]
-    oneLengthMean = dataMean['Length'][dataVariance.index == 1].values[0]
-    oneDiameterMean = dataMean['Diameter'][dataVariance.index == 1].values[0]
-
-    twoFMean = dataMean['F'][dataVariance.index == 2].values[0]
-    twoIMean = dataMean['I'][dataVariance.index == 2].values[0]
-    twoMMean = dataMean['M'][dataVariance.index == 2].values[0]
-    twoLengthMean = dataMean['Length'][dataVariance.index == 2].values[0]
-    twoDiameterMean = dataMean['Diameter'][dataVariance.index == 2].values[0]
-
-    threeFMean = dataMean['F'][dataVariance.index == 3].values[0]
-    threeIMean = dataMean['I'][dataVariance.index == 3].values[0]
-    threeMMean = dataMean['M'][dataVariance.index == 3].values[0]
-    threeLengthMean = dataMean['Length'][dataVariance.index == 3].values[0]
-    threeDiameterMean = dataMean['Diameter'][dataVariance.index == 3].values[0]
-
-    # Variances for 1,2,3
-    oneFVariance = dataVariance['F'][dataVariance.index == 1].values[0]
-    oneIVariance = dataVariance['I'][dataVariance.index == 1].values[0]
-    oneMVariance = dataVariance['M'][dataVariance.index == 1].values[0]
-    oneLengthVariance = dataVariance['Length'][dataVariance.index == 1].values[0]
-    oneDiameterVariance = dataVariance['Diameter'][dataVariance.index == 1].values[0]
-
-    twoFVariance = dataVariance['F'][dataVariance.index == 2].values[0]
-    twoIVariance = dataVariance['I'][dataVariance.index == 2].values[0]
-    twoMVariance = dataVariance['M'][dataVariance.index == 2].values[0]
-    twoLengthVariance = dataVariance['Length'][dataVariance.index == 2].values[0]
-    twoDiameterVariance = dataVariance['Diameter'][dataVariance.index == 2].values[0]
-
-    threeFVariance = dataVariance['F'][dataVariance.index == 3].values[0]
-    threeIVariance = dataVariance['I'][dataVariance.index == 3].values[0]
-    threeMVariance = dataVariance['M'][dataVariance.index == 3].values[0]
-    threeLengthVariance = dataVariance['Length'][dataVariance.index == 3].values[0]
-    threeDiameterVariance = dataVariance['Diameter'][dataVariance.index == 3].values[0]
-
-    result = []
-
-    for i in range(len(test)):
-        try:
-            probabilityOf1 = prior1 * \
-                             p_x_given_y(test['F'][i], oneFMean, oneFVariance) * \
-                             p_x_given_y(test['I'][i], oneIMean, oneIVariance) * \
-                             p_x_given_y(test['M'][i], oneMMean, oneMVariance) * \
-                             p_x_given_y(test['Length'][i], oneLengthMean, oneLengthVariance) * \
-                             p_x_given_y(test['Diameter'][i], oneDiameterMean, oneDiameterVariance)
-
-            probabilityOf2 = prior2 * \
-                             p_x_given_y(test['F'][i], twoFMean, twoFVariance) * \
-                             p_x_given_y(test['I'][i], twoIMean, twoIVariance) * \
-                             p_x_given_y(test['M'][i], twoMMean, twoMVariance) * \
-                             p_x_given_y(test['Length'][i], twoLengthMean, twoLengthVariance) * \
-                             p_x_given_y(test['Diameter'][i], twoDiameterMean, twoDiameterVariance)
-
-            probabilityOf3 = prior3 * \
-                             p_x_given_y(test['F'][i], threeFMean, threeFVariance) * \
-                             p_x_given_y(test['I'][i], threeIMean, threeIVariance) * \
-                             p_x_given_y(test['M'][i], threeMMean, threeMVariance) * \
-                             p_x_given_y(test['Length'][i], threeLengthMean, threeLengthVariance) * \
-                             p_x_given_y(test['Diameter'][i], threeDiameterMean, threeDiameterVariance)
-
-            results = [probabilityOf1, probabilityOf2, probabilityOf3]
-            minimumValue = min(results)
-            if minimumValue == probabilityOf1:
-                output = 1
-            if minimumValue == probabilityOf2:
-                output = 2
-            if minimumValue == probabilityOf3:
-                output = 3
-
-            expectedResult = expectedTest['classlabel'][i]
-
-            result.append([output, expectedResult])
-        except:
-            print(i, test['F'][i], oneFMean, oneFVariance, oneIMean, oneIVariance, oneMMean, oneMVariance)
-
-    return result
-
-
-def p_x_given_y(x, mean_y, variance_y):
-    # Input the arguments into a probability density function
-    exponent = math.exp(-(math.pow(x - mean_y, 2) / (2 * variance_y)))
-    return (1 / (math.sqrt(2 * math.pi * variance_y))) * exponent
-
-
-test = case1Test
-sexHotEncodeTest = pd.get_dummies(test['Sex'])
-test = test.drop('Sex', axis = 1)
-test = test.join(sexHotEncodeTest)
-
-type1(case1Data, case1Test, case1ExpectedTest)
+usedFeaturesType1 = ['F', 'M', 'I', 'Length', 'Diameter']
+usedFeaturesType2 = ['F', 'M', 'I', 'Length', 'Diameter', 'Height','WholeWeight','ShuckedWeight','VisceraWeight','ShellWeight']
+print("Test 1: ")
+naiveBayes(usedFeaturesType1, case1Data, case1Test, case1ExpectedTest)
+print("Test 2: ")
+naiveBayes(usedFeaturesType1, case2Data, case2Test, case2ExpectedTest)
+print("Test 3: ")
+naiveBayes(usedFeaturesType1, case3Data, case3Test, case3ExpectedTest)
+print("Test 4: ")
+naiveBayes(usedFeaturesType2, case4Data, case4Test, case4ExpectedTest)
+print("Test 5: ")
+naiveBayes(usedFeaturesType2, case5Data, case5Test, case5ExpectedTest)
+print("Test 6: ")
+naiveBayes(usedFeaturesType2, case6Data, case6Test, case6ExpectedTest)
